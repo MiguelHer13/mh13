dfdd8d4c94657641627c146d65e11c7c772985b3
MiguelHer13 <u20212201441sco.edu.co>
2023-08-08 18:58:35 -0500

    Clase 08 08 2023

---
+++ input.py
@@ -0,0 +1,17 @@
+nombre = input("Ingrese su nombre :")
+
+print("El dato digitado es ", nombre)
+
+edad = int(input("digite su edad : "))
+edad = edad + 1 
+print(edad)
+
+salario = float(input("ingrese el salario: "))
+print(salario)
+
+#---------
+a = 10 
+b = 2
+
+r = a ** b
+print(r)
